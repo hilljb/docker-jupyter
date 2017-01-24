89a8a62cd419a4f407d8bb5dfbd47a7b7a89c560
Jason Hill <jason@jasonbhill.com>
2017-01-24 22:35:51 +0000

    added base and R containers

---
+++ r-jupyter/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USAGE="usage: run.sh [-d|--datadir datadir] [-w|--workdir workdir]"
+
+
+# Set defaults
+DATADIR="$HOME"
+WORKDIR="$HOME"
+
+#
+while [[ $# -gt 0 ]]; do
+    KEY="$1"
+    case $KEY in
+        -d|--datadir)
+        DATADIR="$2"
+        shift
+        ;;
+        -w|--workdir)
+        WORKDIR="$2"
+        shift
+        ;;
+        -h|help|-help|--help)
+        echo $USAGE
+        exit 0
+        ;;
+        *)
+        ;;
+    esac
+    shift
+done
+
+# remove existing container if it is already running
+if [[ "$(docker ps -a | grep -c "base-jupyter")" -gt "0" ]]; then
+    echo "-- removing existing running container in 5 seconds (Ctrl+c to halt termination)"
+    sleep 5
+    docker rm -f base-jupyter
+fi
+
+# run it
+echo "-- starting container r-jupyter"
+echo "-- Jupyter will give you a URL. (Replace localhost with IP address if needed.)"
+docker run \
+    --name r-jupyter \
+    --rm \
+    -p 8888:8888 \
+    -v $DATADIR:/opt/datadir \
+    -v $WORKDIR:/opt/workdir \
+    -it r-jupyter \
+    bash
